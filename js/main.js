270b0b48f45866b0c82853551def431f17e94d78
Alejo Hincapie <rokneer@gmail.com>
2022-03-06 15:48:45 -0500

    wat

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+  /* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "200px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  } 
